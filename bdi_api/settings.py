5181c20f591b1de928c3a4d2b1c68129096365c6
Luis Cañas <luis_82ec@hotmail.com>
2025-03-19 20:12:18 +0100

    fix of database name

--- bdi_api/settings.py
+++ bdi_api/settings.py
@@ -61,7 +61,7 @@ class DBCredentials:
     """Database credentials loaded from environment variables"""
 
     DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_NAME = os.getenv("DB_NAME", "bdi_aircraft")
+    DB_NAME = os.getenv("DB_NAME", "bdi_aircraft_luiscanas")
     DB_USER = os.getenv("DB_USER", "postgres")
     DB_PASSWORD = os.getenv("DB_PASSWORD", None)
     DB_PORT = int(os.getenv("DB_PORT", 5432))
